09dd1c8349253fa4fd0ce8b6572878c268129d50
nelgoing <ing_nelsongomez@hotmail.com>
2023-05-16 14:44:08 -0500

    Se crea el repositorio con los archivos ya avanzados, ya se había adelantado la información

---
+++ 01_print.py
@@ -0,0 +1,29 @@
+#Para ver la respuesta en la terminal o cómo quedó la linea de código se utiliza el siguiente comando
+print('Hola esto es Python')
+
+#Con print tambíen se puede ver el resultado de operaciones matemáticas
+print(2+5) # suma
+print(2-5) # resta
+print(2*5) # multiplicación
+print(2/5) # división
+print(2//5) # división con valor entero
+print(2**5) # exponenciación
+print(3%15) # modulo o residuo
+
+print(((5//2)+3*4)+25+4-46+500)
+# con 3 comillas se puede digitar una nota de varias lineas y el # sirve para poner una nota en una sola linea
+'''
+Cunado se realizan varias operaciones matemáticas en una sola linea hay que tener en cuenta 
+la regla o el orden en que estás se desarrollan, para eso se puede recordar en una palabra:
+P E M D A S
+1. Parentesis
+2. Exponentes
+3. Multiplicación
+4. División
+5. Adición
+6. Sustracción o Resta
+'''
+
+# Operadores de comparación
+
+
