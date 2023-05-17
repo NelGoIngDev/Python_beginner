73e1083092335861611f484273e44631505a92dd
nelgoing <ing_nelsongomez@hotmail.com>
2023-05-17 18:01:45 -0500

    Se complementa información en el archivo print

--- 01_print.py
+++ 01_print.py
@@ -1,16 +1,32 @@
-#Para ver la respuesta en la terminal o cómo quedó la linea de código se utiliza el siguiente comando
-print('Hola esto es Python')
+
+#En estaprimera parte vamos a ver de forma breve como funciona print, type y los números enteros.
+
+
+'''
+Para ver la respuesta en la terminal o cómo quedó la linea de código se utiliza el comando print
+Las variables se definen fácilmente, asignando un nombre y el valor (Ej: name = 'Rodrigo')
+Para definir los string se utiliza comilla sencilla ('letras') o comilla doble ("letras")
+El comando type se utiliza para ver qué tipo de dato es el que está arrojando el programa
+'''
+
+text = 'Hola mundo'
+print(text)
+print(type(text)) # Rta: class 'str' (str es la abreviatura de string)
 
 #Con print tambíen se puede ver el resultado de operaciones matemáticas
-print(2+5) # suma
-print(2-5) # resta
-print(2*5) # multiplicación
-print(2/5) # división
-print(2//5) # división con valor entero
-print(2**5) # exponenciación
-print(3%15) # modulo o residuo
+suma = 2+5 # Los números o enteros no llevan comillas, de esta manera se le dice al programa el tipo de dato.
+print(suma) # suma
+print(type(suma)) # int es la abreviatura de integer que significa entero en español.
+# Dentro de cada operación se le puede agregar un texto, esto sirve para identificar lo que se ejecuta.
+# Recuerde separarlos con una coma.
+print('Resta =>', 2-5)
+print('Multiplicación =>', 2*5)
+print('División =>', 2/5)
+print('División con valor entero =>', 2//5)
+print('Exponenciación =>', 2**5)
+print('Modulo o residuo =>', 3%15)
 
-print(((5//2)+3*4)+25+4-46+500)
+print('Esta es una operación combinada =>', ((5//2)+3*4)+25+4-46+500)
 # con 3 comillas se puede digitar una nota de varias lineas y el # sirve para poner una nota en una sola linea
 '''
 Cunado se realizan varias operaciones matemáticas en una sola linea hay que tener en cuenta 
@@ -25,5 +41,3 @@ P E M D A S
 '''
 
 # Operadores de comparación
-
-
