c6470025fdd7b4195a9d8ae75a47bbd575f73e89
nelgoing <ing_nelsongomez@hotmail.com>
2023-05-29 17:22:22 -0500

    Se complementa informoción del archivo 02 se anexa inf. de booleanos y transformación de tipo de datos

--- 02_Inf.py
+++ 02_Inf.py
@@ -18,3 +18,24 @@ sean iguales para gerar el Bollean
 is_single = not is_single 
 print(is_single)
 print(type(is_single))
+
+'''
+En la transformación de tipos de datos, podemos convertir un numero en texto, un texto en número o un string en boleano.
+print('Ricardo tiene ' + 12 'años') Esto Python no lo va a saber interpretar ya que Python no sabe qué hacer con un 
+string y un número entero "int" Entonces veamos los siguientes ejemplos.
+'''
+name = 'Ricardo'
+print(type(name)) # Rta. class 'str' = texto
+last_name = 'Quintero Rodriguez'
+print(type(last_name)) # Rta. class 'str' = texto
+age = 18
+print(type(age)) # Rta. class 'int' = 'número'
+married = True
+print(type(married)) # Rta. class 'bool' = boleano
+
+# Hay dos maneras de convertir los tipos de datos: concatenando con el signo + y utilizando la función de formato f
+template = 'v1 = Hola, mi nombre es ' + name + ' y mis apellidos son ' + last_name + ' mi edad es ' + str(age) + ' y mi estado civil es casado ' + str(married)
+print(template)
+
+template = f'v2 = Hola, mi nombre es {name} y mis apellidos son {last_name} mi edad es {age} y mi estado civil es casado {married}'
+print(template)
