00a715650b12527f8f41faac4f12d11318951a5a
nelgoing <ing_nelsongomez@hotmail.com>
2023-08-23 17:48:51 -0500

    Se complementa el contenido del archivo 05

--- 05_Inf.py
+++ 05_Inf.py
@@ -20,6 +20,13 @@ my_print('Este es mi texto')
 my_print('No hay que volver a escribir la logica definida en la función, solo hay que ejecutarla y ella hará nuevamente lo definido. ')
 
 # Podemos definir operaciones matemáticas.
+# Una operación matematica tradicional sería así:
+a = 1
+b = 2
+suma = a + b
+print(suma)
+
+# La operación suma con utilizando función sería de la siguiente manera
 def suma(a, b):
     print(a + b)
 
@@ -32,3 +39,35 @@ def suma(a, b):
 
 suma(1, 6) # 14 lo que hace es ejecutar la multiplicación con los números.
 suma(20, 50) # 140
+
+snake = '''
+       /^\/^\\
+     _|__|  O|
+\/ \/   /~     \/
+ \____|_________/
+        \_______)
+        __//___//
+       (_______(
+'''
+print(snake) # Esto es para separar el tema al ejecutar los resultados en la terminal
+
+# Función return, para explicar esta sección veremos la forma tradicional y después aplicando funciones
+suma = 0
+for x in range(1, 10): # Le decimos al programa que sume consecutivamente desde el 1 hasta el 10 (1+2+3+4+5+6+7+8+9+10 = 45)
+    suma += x
+print(suma)
+
+# Ahora aplicando funciones sería de la siguiente manera
+def sum_with_range(min, max): # Podemos asignar parámetros min y max
+    print(min, max) # Se imprime los rangos sin el resultado
+    sum = 0
+    for x in range(min, max):
+        sum += x
+    return sum # Retorna la suma con el calculo total
+
+result = sum_with_range(10, 60) # de esta manera podemos ejecutar la misma logica solo con llamar la función y asignarle parámetros
+print(result) # Con esto estamos imprimiendo el resultado 
+result_2 = sum_with_range(result, result + 1000) # acá utilizamos el resultado anterior como rango y le indicamos que al resultado anterior le sume 1.000
+print(result_2)
+
+
